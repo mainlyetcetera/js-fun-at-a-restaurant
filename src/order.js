0839b1c41c7f2130566704d8901d78c7cb9b11c6
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-08 17:14:14 -0600

    Write function that passes listItems tests

--- src/order.js
+++ src/order.js
@@ -6,9 +6,44 @@ function refundOrder(orderNumber, arrOfOrders) {
   arrOfOrders.splice(orderNumber - 1, 1);
 }
 
-function listItems() {
+function listItems(arrOfOrders) {
+  var result, arrOfItems;
+  result = '';
 
+  arrOfItems = arrOfOrders.map(function(element) {
+    return element.item;
+  });
+
+  arrOfItems.forEach(function(element, index) {
+    index === arrOfItems.length - 1 ? result += element : result += element + ', ';
+  });
+
+  return result;
 }
+
+var input = [
+{
+  orderNumber: 1,
+  item: "burger",
+  price: "8.99",
+  orderType: "delivery",
+  status: "accepted"
+},
+{
+  orderNumber: 2,
+  item: "blt sandwich",
+  price: "5.99",
+  orderType: "delivery",
+  status: "accepted"
+},
+{
+  orderNumber: 3,
+  item: "rueben",
+  price: "8.99",
+  orderType: "delivery",
+  status: "accepted"
+}];
+console.log(listItems(input));
 
 function searchOrder() {
 
