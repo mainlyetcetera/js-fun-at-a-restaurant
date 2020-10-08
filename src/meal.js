e3b413af569a97fdc51ad5be33e135a32327c701
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-08 14:48:59 -0600

    Write code passing nameMenuItem and createMenuItem tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,27 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 }
+
+nameMenuItem('Burrito');
+nameMenuItem('Pizza');
+nameMenuItem('Taco');
+
+
+
+function createMenuItem(itemName, itemPrice, itemType) {
+  return { name : nameMenuItem(itemName), price : itemPrice, type : itemType };
+}
+
+// var menuItem = nameMenuItem('French Toast');
+console.log(createMenuItem('French Toast', 10.99, "breakfast"));
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
