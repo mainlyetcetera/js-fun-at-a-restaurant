6d1fdcc60a297a6228eae3f90c5d1f03550da7e9
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-08 15:45:21 -0600

    Make first function declarations, prep for tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,22 @@
+function takeOrder() {
+
+}
+
+function refundOrder() {
+
+}
+
+function listItems() {
+
+}
+
+function searchOrder() {
+
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
