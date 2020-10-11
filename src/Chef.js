716620d124efb3dd7545505e64895da41dc59a97
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-11 10:44:30 -0600

    Write chef class to pass all tests

--- src/Chef.js
+++ src/Chef.js
@@ -12,6 +12,17 @@ class Chef {
     }
     // return isMorning ? `Good morning, ${name}!` : `Hello, ${name}!`
   }
+
+  checkForFood(foodItem) {
+    var foodItemType = foodItem.type;
+    if (this.restaurant.menus[foodItemType].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`;
+    }
+    // return this.restaurant.menus[foodItemType].includes(foodItem) ? `Yes, we're serving ${foodItem.name} today!` : `Sorry, we aren't serving ${foodItem.name} today.`;
+  }
+
 }
 
 module.exports = Chef;
