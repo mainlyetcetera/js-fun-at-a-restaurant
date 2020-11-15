60df64e86cd6e71643e61f3d3d18e7a24ffbbdd6
mainlyetcetera <ericcampbell294@gmail.com>
2020-11-15 16:46:23 -0700

    Refactor createRecipe function to be arrow function

--- src/meal.js
+++ src/meal.js
@@ -3,10 +3,7 @@ const createMenuItem = (name, price, type) => ({name, price, type});
 const addIngredients = (ingredient, ingredients) => !ingredients.includes(ingredient) ? ingredients.push(ingredient) : ingredient;
 const formatPrice = price => `$${price}`;
 const decreasePrice = price => price *= .9;
-
-function createRecipe(menuItemTitle, menuItemIngredients, menuItemType) {
-  return { title : menuItemTitle, ingredients : menuItemIngredients, type : menuItemType };
-}
+const createRecipe = (title, ingredients, type) => ({title, ingredients, type});
 
 module.exports = {
   nameMenuItem,
