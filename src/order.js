fbbc1e2f7775f47a14f7c0346057bbf4a43b8437
mainlyetcetera <ericcampbell294@gmail.com>
2020-11-15 17:04:23 -0700

    Refactor listItems funcction to be arrow function

--- src/order.js
+++ src/order.js
@@ -1,28 +1,21 @@
 const takeOrder = (order, orders) => orders.length < 3 ? orders.push(order) : orders;
 const refundOrder = (orderNumber, orders) => orders.splice(orderNumber - 1, 1);
-
-function listItems(arrOfOrders) {
+const listItems = orders => {
   var result, arrOfItems;
   result = '';
   arrOfItems = [];
 
-  for (var i = 0; i < arrOfOrders.length; i++) {
-    arrOfItems.push(arrOfOrders[i].item);
+  for (var i = 0; i < orders.length; i++) {
+    arrOfItems.push(orders[i].item);
   }
-  // arrOfItems = arrOfOrders.map(function(element) {
-  //   return element.item;
-  // });
 
-  for (var j = 0; j < arrOfItems.length; j++) {
+  for (var j = 0; j < orders.length; j++) {
     if (j === arrOfItems.length - 1) {
       result += arrOfItems[j];
     } else {
       result += arrOfItems[j] + ', ';
     }
   }
-  // arrOfItems.forEach(function(element, index) {
-  //   index === arrOfItems.length - 1 ? result += element : result += element + ', ';
-  // });
 
   return result;
 }
